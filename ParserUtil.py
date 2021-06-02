864ee66e84987d859a23a5cc4facce6d39c77995
Rajah Chacko <rajah@alumni.duke.edu>
2021-06-02 12:05:05 -0400

    SasParser.py: (moved from ParserUtil). ParserUtil.py: moved out SasParser and made it a test class.

--- ParserUtil.py
+++ ParserUtil.py
@@ -42,6 +42,8 @@ class ParserUtil:
             yacc.parse(s)
 
     # Token rules that don't need to be reimplemented.
+    t_ignore = ' \t'
+
     def t_newline(self, t):
         r'\n+'
         t.lexer.lineno += t.value.count('\n')
@@ -57,84 +59,3 @@ class ParserUtil:
         else:
             logger.error('Syntax error at EOF')
 
-
-class Sas(ParserUtil):
-    # All keywords must be uppercase
-    # Helpful to have the following tuple end in an extra comma.
-    keywords = (
-        'PROC',
-        'MEANS',
-        'DATA',
-        'EOL',
-        'RUN',
-    )
-    # Helpful to have the following tuple end in an extra comma.
-    tokens = keywords + (
-        'DATASETNAME',
-        'EQUALS', 
-#        'ID',
-    )
-
-    # Tokens
-    t_EOL = ';'
-    t_ignore = ' \t'
-    t_EQUALS = '='
-
-    def t_DATASETNAME(self, t):
-        r'[A-Za-z][A-Za-z0-9\.]*'
-        tupper = str(t.value).upper()
-        if tupper in self.keywords:
-            t.type = tupper
-            logger.debug(f'interpreting as keyword: {t.value}')
-        else:
-            logger.debug(f'encountering datasetname: {t.value}')
-        return t
-
-
-    # Parsing rules
-    def p_statement_proc(self, p):
-        '''
-        statement : procdecl
-        '''
-
-    def p_procdecl(self, p):
-        '''
-        procdecl : procmeans
-        '''
-
-    def p_procmeans(self, p):
-        '''
-        procmeans : procmeansdecl procend
-        '''
-
-    def p_proc_means_decl(self, p):
-        '''
-        procmeansdecl : PROC MEANS EOL
-                      | PROC MEANS procoptions EOL
-        '''
-
-    def p_proc_options(self, p):
-        '''
-        procoptions : procoptions procoption
-                    | procoption
-        '''
-
-    def p_proc_option(self, p):
-        '''
-        procoption : dataoption
-        '''
-
-    def p_data_option(self, p):
-        '''
-        dataoption : DATA EQUALS DATASETNAME
-        '''
-
-    def p_proc_end(self, p):
-        '''
-        procend : RUN EOL
-        '''
-        logger.debug('encountered RUN statement')
-
-if __name__ == '__main__':
-    s = Sas()
-    s.run()
